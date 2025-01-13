7780328692a075d8889cc90975bd40d9d8061958
Sahil Srivastava <149762467+Sahil1607cms@users.noreply.github.com>
2025-01-13 23:08:48 +0530

    Update App.jsx

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,12 +5,16 @@ import './App.css'
 
 function App() {
 
+  //first parameter is variable name and second parameter is method name, useState is hook
+  //we use hook so that the page renders with updated value automatically 
   const [counter, setCounter]  = useState(15)
 
   //let counter = 15
 
   const addValue = () => {
     //counter = counter + 1
+    //using this prevCounter call back the setCounter will be executed eeach time it is written
+    //if we dont use callback, the whole lines of setCounter will be packed as bundle as treated as single statement 
     setCounter(prevCounter => prevCounter + 1)
     setCounter(prevCounter => prevCounter + 1 )
     setCounter(prevCounter => prevCounter + 1)
@@ -23,6 +27,7 @@ function App() {
   }
   
   return (
+    //modern conventions dont require us to use divs unnecessarily, we can directly return the section paranthesis 
     <>
       <h1>Chai aur react</h1>
       <h2>Counter value: {counter}</h2>
