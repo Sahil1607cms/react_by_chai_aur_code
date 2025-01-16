8bac50cf13e6ba54e96fc1c8a594f59866f622a5
Sahil Srivastava <149762467+Sahil1607cms@users.noreply.github.com>
2025-01-16 22:55:08 +0530

    Update useCurrencyInfo.js

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,14 +1,16 @@
 import {useEffect, useState} from "react"
 
-
+//creating custom hook with a parameter, this hook will return a data
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
+    //whenever this hook is loaded, the api should be called, for this we will use useEffect
+    //useEffect requres a callback and dependency
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
+        .then((res) => res.json()) //converting to json 
+        .then((res) => setData(res[currency])) //holding the data in data, can also use res.currency using . operator
         console.log(data);
-    }, [currency])
+    }, [currency])//whemeber there is chage in currency call this hook
     console.log(data);
     return data
 }
